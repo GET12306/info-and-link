1f1649b3b9b9ffdcfea89e1d0014262e76db2649
加藤恵 <megumi@katoumegumi.local>
2026-05-13 21:20:33 +0900

    put data into yaml file so it's easy to modify

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export interface Role {
+  year: string;
+  title: { ja: string; en: string };
+  character: { ja: string; en: string };
+}
+
+export interface Activity {
+  category: "Stage" | "Musical" | "Program" | "Live" | "Other";
+  date: string;
+  title: { ja: string; en: string };
+  description?: { ja: string; en: string };
+  link: string;
+}
+
+export interface FanProject {
+  title: { ja: string; en: string };
+  organizer: string;
+  organizerUrl?: string;
+  url: string;
+  description: { ja: string; en: string };
+}
+
+export interface LinkItem {
+  platform: { ja: string; en: string };
+  url: string;
+  description: { ja: string; en: string };
+  icon: string;
+}
